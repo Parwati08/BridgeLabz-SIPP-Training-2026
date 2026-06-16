53a1e57ffc929c9eba26c60fd1d76bcbd5717f90
Parwati <parwatisaraswat005@gmail.com>
2026-06-16 13:05:27 +0530

    [Parwati] add:Scenario based question

---
+++ core-java-pratice/gcr-codebase/Scenario/KingdomTaxSystem.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class KingdomTaxSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double[] incomes = new double[10];
+        double totalTaxCollected = 0;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter income of citizen " + (i + 1) + ": ");
+            incomes[i] = sc.nextDouble();
+
+            double tax;
+            String bracket;
+
+            if (incomes[i] < 10000) {
+                tax = incomes[i] * 0.05;
+                bracket = "5% Tax Bracket";
+            } 
+            else if (incomes[i] <= 50000) {
+                tax = incomes[i] * 0.15;
+                bracket = "15% Tax Bracket";
+            } 
+            else {
+                tax = incomes[i] * 0.30;
+                bracket = "30% Tax Bracket";
+            }
+
+            System.out.println("Tax Bracket : " + bracket);
+            System.out.println("Tax Amount  : ₹" + tax);
+            System.out.println();
+
+            totalTaxCollected += tax;
+        }
+
+        System.out.println("================================");
+        System.out.println("Total Tax Collected = ₹" + totalTaxCollected);
+        System.out.println("================================");
+
+        sc.close();
+    }
+}
--- core-java-pratice/gcr-codebase/Scenario/wordmatchsystem.java
+++ core-java-pratice/gcr-codebase/Scenario/wordmatchsystem.java
@@ -4,24 +4,18 @@ import java.util.Scanner;
 public class wordmatchsystem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // Accept two strings
         System.out.print("Enter first string: ");
         String str1 = sc.nextLine();
-
         System.out.print("Enter second string: ");
         String str2 = sc.nextLine();
 
-        // Display strings in uppercase
         System.out.println("\nStrings in Uppercase:");
         System.out.println("String 1: " + str1.toUpperCase());
         System.out.println("String 2: " + str2.toUpperCase());
 
-        // Display lengths
         System.out.println("\nLength of String 1: " + str1.length());
         System.out.println("Length of String 2: " + str2.length());
 
-        // Check for anagram
         String s1 = str1.replaceAll("\\s", "").toLowerCase();
         String s2 = str2.replaceAll("\\s", "").toLowerCase();
 
